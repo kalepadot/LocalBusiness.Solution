656adc096e431e3226e281ec1eeacfd240930762
Jeremy Kale Padot <jeremy@padot.us>
2020-04-03 16:08:08 -0700

    continue to figure out nswag, not working

--- LocalBusiness/Startup.cs
+++ LocalBusiness/Startup.cs
@@ -24,7 +24,6 @@ namespace LocalBusiness
 
         public IConfiguration Configuration { get; }
 
-            // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
            
@@ -46,11 +45,9 @@ namespace LocalBusiness
             else
             {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
-            // app.UseHttpsRedirection();
             app.UseStaticFiles();
             app.UseCookiePolicy();
             app.UseSwagger();
@@ -58,7 +55,6 @@ namespace LocalBusiness
               app.UseSwaggerUI(c =>
             {
                 c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
-                // c.RoutePrefix = string.Empty;
             });
 
 
