0ef4e2ee44df2360035ce76473ea7f2d3fc97d7a
Jeremy Kale Padot <jeremy@padot.us>
2020-04-03 11:06:39 -0700

    seed data update

---
+++ LocalBusiness/Migrations/20200403180558_SeedData.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace LocalBusiness.Migrations
+{
+    public partial class SeedData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Restaurants",
+                columns: new[] { "RestaurantId", "Name", "Type" },
+                values: new object[,]
+                {
+                    { 1, "Harus Bakery", "Bakery" },
+                    { 2, "Harus Cafe", "Cafe" },
+                    { 3, "House of Haru", "Sushi" },
+                    { 4, "Ristorante Harus", "Italian" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Restaurants",
+                keyColumn: "RestaurantId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Restaurants",
+                keyColumn: "RestaurantId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Restaurants",
+                keyColumn: "RestaurantId",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Restaurants",
+                keyColumn: "RestaurantId",
+                keyValue: 4);
+        }
+    }
+}
