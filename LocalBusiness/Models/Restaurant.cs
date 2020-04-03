3f60b7ac856e90c16ed91ae12ccd28ad4a11f1cb
Jeremy Kale Padot <jeremy@padot.us>
2020-04-03 11:32:25 -0700

    add some validations

--- LocalBusiness/Models/Restaurant.cs
+++ LocalBusiness/Models/Restaurant.cs
@@ -1,9 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace LocalBusiness.Models
+
 {
     public class Restaurant
     {
         public int RestaurantId { get; set; }
+        [Required]
+        [StringLength(30)]
         public string Name { get; set; }
+        [Required]
         public string Type { get; set; }
         // public int Age { get; set; }
         // public string Gender { get; set; }
