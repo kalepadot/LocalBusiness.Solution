a58a895ac994f45d1c122b69dd172874ba5467dc
Jeremy Kale Padot <jeremy@padot.us>
2020-04-03 10:33:38 -0700

    create seed database entries

--- LocalBusiness/Models/LocalBusinessContext.cs
+++ LocalBusiness/Models/LocalBusinessContext.cs
@@ -10,5 +10,15 @@ namespace LocalBusiness.Models
         }
 
         public DbSet<Restaurant> Restaurants { get; set; }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+          builder.Entity<Restaurant>()
+            .HasData(
+          new Restaurant { RestaurantId = 1, Name = "Harus Bakery", Type= "Bakery" },
+          new Restaurant { RestaurantId = 2, Name = "Harus Cafe", Type= "Cafe" },
+          new Restaurant { RestaurantId = 3, Name = "House of Haru", Type= "Sushi" },
+          new Restaurant { RestaurantId = 4, Name = "Ristorante Harus", Type= "Italian" }
+          );
+        }
     }
 }
